254ca026333f5e5e23eed5ed2b02aa927b3d012f
baalperez <85808684+baalperez@users.noreply.github.com>
2022-02-19 16:42:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-   const apiRoot = 'https://kodillacrud.herokuapp.com/v1/task/';
-   const trelloApiRoot = 'https://kodillacrud.herokuapp.com/v1/trello/';
+   const apiRoot = 'https://kodilla-tasks-bp.herokuapp.com/v1/task/';
+   const trelloApiRoot = 'https://kodilla-tasks-bp.herokuapp.com/v1/trello/';
    const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
    const $tasksContainer = $('[data-tasks-container]');
 
